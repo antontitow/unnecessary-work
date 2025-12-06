22697999f145156e3d2efd767c4652ffca295c53
anton <antontitow@bk.ru>
2025-12-06 23:41:21 +0300

    Свагер

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
     implementation("org.slf4j:slf4j-api:2.0.9")
     implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 }
 
 tasks.test {
--- src/main/kotlin/ru/tina/controller/MessageController.kt
+++ src/main/kotlin/ru/tina/controller/MessageController.kt
@@ -1,20 +1,39 @@
 package ru.tina.controller
 
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ru.tina.objects.Encryption
 import ru.tina.objects.First
 
 @RestController
+@RequestMapping("/api/v1")
+@Tag(name = "Crypto Messaging API", description = "API для криптографических операций с сообщениями")
 class MessageController : Encryption {
 
-    override fun getHash(@RequestParam message: String?): String {
+    @Operation(summary = "Получить хэш сообщения")
+    override fun getHash(
+        @Parameter(
+            description = "Сообщение для шифрования",
+            required = false
+        )
+        @RequestParam message: String?
+    ): String {
         var first = First(message)
         return first.getHash();
     }
 
-    override fun getBase64(@RequestParam message: String?): String {
+    @Operation(summary = "Получить Base64 сообщения")
+    override fun getBase64(
+        @Parameter(
+            description = "Сообщение для шифрования",
+            required = false
+        ) @RequestParam message: String?
+    ): String {
         var first = First(message)
         return first.getBase64();
     }
